d89ff5e2f7ecf9e238a15b26c9171526432beb87
GSarpong4423 <sarpongg@gmail.com>
2018-12-24 03:21:58 +0000

    global objects

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-console.log(__dirname);
-console.log(__filename);
+console.log(__dirname) - find directory name
+console.log(__filename) - find file name
+
+
+// var time = 0;
+
+// var timer = setInterval(function(){
+//     time += 2;
+//     console.log(time + 'seconds have passed');
+//     if (time > 5){
+//         clearInterval(timer);
+//     }
+// }, 2000);
 
 
     
